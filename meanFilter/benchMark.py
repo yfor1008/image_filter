85638f17e5395fcf2e63b4f4939d051cf1fcfead
yfor <yfor1008@gmail.com>
2021-12-19 13:53:56 +0800

    [feat] 添加 kuwaraha 实现

--- meanFilter/benchMark.py
+++ meanFilter/benchMark.py
@@ -12,19 +12,18 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import time
 import numpy as np
-from meanFilter import meanFilterBase, meanFilterSat
+from meanFilter import meanFilterBase, meanFilterSat, meanFilterSWF, meanFilterSNN, meanFilterKuwahara
 
-if __name__ == '__main__':
-
+def time_test():
+    '''耗时测试'''
     img_path = '../src/lena.jpg'
     im = Image.open(img_path)
     im = np.array(im, dtype=float)
 
-    filter_size = [2,4,8,16,32,64]
+    filter_size = [1,2,4,8,16]
+
     time_base = np.zeros((len(filter_size,)))
-    time_sat = np.zeros((len(filter_size,)))
     mean_base = np.zeros(im.shape, im.dtype)
-    mean_sat = np.zeros(im.shape, im.dtype)
     for idx, fs in enumerate(filter_size):
         time_start = time.perf_counter()
         mean_base[:,:,0] = meanFilterBase(im[:,:,0], fs)
@@ -32,7 +31,10 @@ if __name__ == '__main__':
         mean_base[:,:,2] = meanFilterBase(im[:,:,2], fs)
         time_end = time.perf_counter()
         time_base[idx] = time_end - time_start
-        
+
+    time_sat = np.zeros((len(filter_size,)))
+    mean_sat = np.zeros(im.shape, im.dtype)
+    for idx, fs in enumerate(filter_size):
         time_start = time.perf_counter()
         mean_sat[:,:,0] = meanFilterSat(im[:,:,0], fs)
         mean_sat[:,:,1] = meanFilterSat(im[:,:,1], fs)
@@ -40,11 +42,106 @@ if __name__ == '__main__':
         time_end = time.perf_counter()
         time_sat[idx] = time_end - time_start
 
+    time_swf = np.zeros((len(filter_size,)))
+    mean_swf = np.zeros(im.shape, im.dtype)
+    for idx, fs in enumerate(filter_size):
+        time_start = time.perf_counter()
+        mean_swf[:,:,0] = meanFilterSWF(im[:,:,0], fs)
+        mean_swf[:,:,1] = meanFilterSWF(im[:,:,1], fs)
+        mean_swf[:,:,2] = meanFilterSWF(im[:,:,2], fs)
+        time_end = time.perf_counter()
+        time_swf[idx] = time_end - time_start
+
+    # time_snn = np.zeros((len(filter_size,)))
+    # mean_snn = np.zeros(im.shape, im.dtype)
+    # for idx, fs in enumerate(filter_size):
+    #     time_start = time.perf_counter()
+    #     mean_snn[:,:,0] = meanFilterSNN(im[:,:,0], fs)
+    #     mean_snn[:,:,1] = meanFilterSNN(im[:,:,1], fs)
+    #     mean_snn[:,:,2] = meanFilterSNN(im[:,:,2], fs)
+    #     time_end = time.perf_counter()
+    #     time_snn[idx] = time_end - time_start
+
+    time_kuwahara = np.zeros((len(filter_size,)))
+    mean_kuwahara = np.zeros(im.shape, im.dtype)
+    for idx, fs in enumerate(filter_size):
+        time_start = time.perf_counter()
+        mean_kuwahara[:,:,0] = meanFilterKuwahara(im[:,:,0], fs)
+        mean_kuwahara[:,:,1] = meanFilterKuwahara(im[:,:,1], fs)
+        mean_kuwahara[:,:,2] = meanFilterKuwahara(im[:,:,2], fs)
+        time_end = time.perf_counter()
+        time_kuwahara[idx] = time_end - time_start
+
     plt.plot(filter_size, time_base)
     plt.plot(filter_size, time_sat)
+    plt.plot(filter_size, time_swf)
+    # plt.plot(filter_size, time_snn)
+    plt.plot(filter_size, time_kuwahara)
     plt.title('Time Consuming', fontsize=20)
     plt.xlabel('filter window radius', fontsize=14)
     plt.ylabel('time(s)', fontsize=14)
-    plt.legend(['base', 'integral'])
+    # plt.legend(['base', 'sat', 'swf', 'snn', 'kuwahara'])
+    plt.legend(['base', 'sat', 'swf', 'kuwahara'])
     plt.savefig('../src/mean_filter_time_cmp.png')
     plt.show()
+
+def effect_test():
+    '''效果测试'''
+
+    img_path = '../src/lena.jpg'
+    im = Image.open(img_path).resize((256,256))
+    im = np.array(im, dtype=float)
+
+    radius = 3
+    run_times = 3
+    
+    mean_base = Image.open(img_path).resize((256,256))
+    mean_base = np.array(mean_base, dtype=float)
+    for i in range(run_times):
+        mean_base[:,:,0] = meanFilterBase(mean_base[:,:,0], radius)
+        mean_base[:,:,1] = meanFilterBase(mean_base[:,:,1], radius)
+        mean_base[:,:,2] = meanFilterBase(mean_base[:,:,2], radius)
+
+    mean_sat = Image.open(img_path).resize((256,256))
+    mean_sat = np.array(mean_sat, dtype=float)
+    for i in range(run_times):
+        mean_sat[:,:,0] = meanFilterSat(mean_sat[:,:,0], radius)
+        mean_sat[:,:,1] = meanFilterSat(mean_sat[:,:,1], radius)
+        mean_sat[:,:,2] = meanFilterSat(mean_sat[:,:,2], radius)
+
+    mean_swf = Image.open(img_path).resize((256,256))
+    mean_swf = np.array(mean_swf, dtype=float)
+    for i in range(run_times):
+        mean_swf[:,:,0] = meanFilterSWF(mean_swf[:,:,0], radius)
+        mean_swf[:,:,1] = meanFilterSWF(mean_swf[:,:,1], radius)
+        mean_swf[:,:,2] = meanFilterSWF(mean_swf[:,:,2], radius)
+
+    mean_snn = Image.open(img_path).resize((256,256))
+    mean_snn = np.array(mean_snn, dtype=float)
+    for i in range(run_times):
+        mean_snn[:,:,0] = meanFilterSNN(mean_snn[:,:,0], radius)
+        mean_snn[:,:,1] = meanFilterSNN(mean_snn[:,:,1], radius)
+        mean_snn[:,:,2] = meanFilterSNN(mean_snn[:,:,2], radius)
+
+    mean_kuwahara = Image.open(img_path).resize((256,256))
+    mean_kuwahara = np.array(mean_kuwahara, dtype=float)
+    for i in range(run_times):
+        mean_kuwahara[:,:,0] = meanFilterKuwahara(mean_kuwahara[:,:,0], radius)
+        mean_kuwahara[:,:,1] = meanFilterKuwahara(mean_kuwahara[:,:,1], radius)
+        mean_kuwahara[:,:,2] = meanFilterKuwahara(mean_kuwahara[:,:,2], radius)
+
+    im_pair1 = np.concatenate((im, mean_base, mean_sat), axis=1)
+    im_pair2 = np.concatenate((mean_swf, mean_snn, mean_kuwahara), axis=1)
+    im_pair = np.concatenate((im_pair1, im_pair2), axis=0)
+    im_pair = Image.fromarray(im_pair.astype('uint8'))
+    im_pair.save('../src/original-base-sat-swf-snn-kuwahara.png')
+
+    plt.imshow(im_pair)
+    plt.title('effect of numerical filter:\noriginal, base, sat, swf, snn, kuwahara', fontsize=14)
+    plt.axis('off')
+    plt.show()
+
+if __name__ == '__main__':
+
+    time_test()
+    # effect_test()
--- meanFilter/meanFilter.py
+++ meanFilter/meanFilter.py
@@ -100,7 +100,7 @@ def meanFilterSWF(im, radius):
     im_pad = np.zeros((im_h+2*radius+1, im_w+2*radius+1), im.dtype)
     im_pad[1:, 1:] = padding(im, radius)
 
-    # 计算积分图, nums用于记录每个block像素个数
+    # 计算积分图
     sat = np.cumsum(im_pad, axis=0)
     sat = np.cumsum(sat, axis=1)
 
@@ -173,6 +173,65 @@ def meanFilterSNN(im, radius):
             filtered[r-radius, c-radius] = np.mean(nn)
     return filtered
 
+def meanFilterKuwahara(im, radius):
+    '''
+    ### Docs: kuwahara均值滤波, 使用积分图加速
+        - "Smooth operator: Smoothing seismic interpretations and attributes", 2007
+        - 方差: 转换成一阶及二阶积分图, sum(xi^2)/n - u^2
+    ### Args:
+        - im: H*W, numpy.array, float, 单通道图像数据
+        - radius: int, 滤波窗口半径
+    ### Returns:
+        - filtered: 与 im 相同格式
+    ### Examples:
+    '''
+
+    im_h, im_w = im.shape
+    blk_size = (radius+1) * (radius+1)
+
+    # 向外扩充, 前面左边多增加1行1列, 方便计算
+    im_pad = np.zeros((im_h+2*radius+1, im_w+2*radius+1), im.dtype)
+    im_pad[1:, 1:] = padding(im, radius)
+    im_pad2 = im_pad ** 2
+
+    # 计算积分图
+    sat = np.cumsum(im_pad, axis=0)
+    sat = np.cumsum(sat, axis=1)
+    sat2 = np.cumsum(im_pad2, axis=0)
+    sat2 = np.cumsum(sat2, axis=1)
+
+    # 滤波
+    filtered = np.zeros((im_h, im_w), im.dtype)
+    for r in range(radius+1, im_h+radius+1):
+        bottom_r = r+radius
+        top_r = r-radius-1
+        for c in range(radius+1, im_w+radius+1):
+            right_c = c+radius
+            left_c = c-radius-1
+            blk_LU_std = sat2[r, c] - sat2[top_r, c] - sat2[r, left_c] + sat2[top_r, left_c]
+            blk_LU = sat[r, c] - sat[top_r, c] - sat[r, left_c] + sat[top_r, left_c]
+            blk_LU_std = blk_LU_std - blk_LU ** 2 / blk_size
+
+            blk_LD_std = sat2[bottom_r, c] - sat2[r-1, c] - sat2[bottom_r, left_c] + sat2[r-1, left_c]
+            blk_LD = sat[bottom_r, c] - sat[r-1, c] - sat[bottom_r, left_c] + sat[r-1, left_c]
+            blk_LD_std = blk_LD_std - blk_LD ** 2 / blk_size
+
+            blk_RD_std = sat2[bottom_r, right_c] - sat2[r-1, right_c] - sat2[bottom_r, c-1] + sat2[r-1, c-1]
+            blk_RD = sat[bottom_r, right_c] - sat[r-1, right_c] - sat[bottom_r, c-1] + sat[r-1, c-1]
+            blk_RD_std = blk_RD_std - blk_RD ** 2 / blk_size
+
+            blk_RU_std = sat2[r, right_c] - sat2[top_r, right_c] - sat2[r, c-1] + sat2[top_r, c-1]
+            blk_RU = sat[r, right_c] - sat[top_r, right_c] - sat[r, c-1] + sat[top_r, c-1]
+            blk_RU_std = blk_RU_std - blk_RU ** 2 / blk_size
+
+            stds = [blk_LU_std, blk_LD_std, blk_RD_std, blk_RU_std]
+            means = [blk_LU, blk_LD, blk_RD, blk_RU]
+            idx = np.argmin(stds)
+            
+            filtered[r-radius-1, c-radius-1] = means[idx]
+    filtered = filtered / blk_size
+    return filtered
+
 if __name__ == '__main__':
 
     from PIL import Image
@@ -182,30 +241,13 @@ if __name__ == '__main__':
     im = Image.open(img_path)
     im = np.array(im, dtype=float)
 
-    mean_base = np.zeros(im.shape, im.dtype)
-    mean_sat = np.zeros(im.shape, im.dtype)
-    mean_swf = np.zeros(im.shape, im.dtype)
-    mean_snn = np.zeros(im.shape, im.dtype)
-
     radius = 3
 
+    # base
+    mean_base = np.zeros(im.shape, im.dtype)
     mean_base[:,:,0] = meanFilterBase(im[:,:,0], radius)
     mean_base[:,:,1] = meanFilterBase(im[:,:,1], radius)
     mean_base[:,:,2] = meanFilterBase(im[:,:,2], radius)
-
-    mean_sat[:,:,0] = meanFilterSat(im[:,:,0], radius)
-    mean_sat[:,:,1] = meanFilterSat(im[:,:,1], radius)
-    mean_sat[:,:,2] = meanFilterSat(im[:,:,2], radius)
-
-    mean_swf[:,:,0] = meanFilterSWF(im[:,:,0], radius)
-    mean_swf[:,:,1] = meanFilterSWF(im[:,:,1], radius)
-    mean_swf[:,:,2] = meanFilterSWF(im[:,:,2], radius)
-
-    mean_snn[:,:,0] = meanFilterSNN(im[:,:,0], radius)
-    mean_snn[:,:,1] = meanFilterSNN(im[:,:,1], radius)
-    mean_snn[:,:,2] = meanFilterSNN(im[:,:,2], radius)
-
-    assert (mean_base == mean_sat).all()
 
     plt.figure()
     im_base = Image.fromarray(mean_base.astype('uint8'))
@@ -213,21 +255,51 @@ if __name__ == '__main__':
     plt.axis('off')
     plt.show()
 
+    # 积分图
+    mean_sat = np.zeros(im.shape, im.dtype)
+    mean_sat[:,:,0] = meanFilterSat(im[:,:,0], radius)
+    mean_sat[:,:,1] = meanFilterSat(im[:,:,1], radius)
+    mean_sat[:,:,2] = meanFilterSat(im[:,:,2], radius)
+
     plt.figure()
     im_sat = Image.fromarray(mean_sat.astype('uint8'))
     plt.imshow(im_sat)
     plt.axis('off')
     plt.show()
 
+    # 侧窗
+    mean_swf = np.zeros(im.shape, im.dtype)
+    mean_swf[:,:,0] = meanFilterSWF(im[:,:,0], radius)
+    mean_swf[:,:,1] = meanFilterSWF(im[:,:,1], radius)
+    mean_swf[:,:,2] = meanFilterSWF(im[:,:,2], radius)
+
     plt.figure()
-    im_swf = Image.fromarray(mean_snn.astype('uint8'))
+    im_swf = Image.fromarray(mean_swf.astype('uint8'))
     plt.imshow(im_swf)
     plt.axis('off')
     plt.show()
 
+    # SNN
+    mean_snn = np.zeros(im.shape, im.dtype)
+    mean_snn[:,:,0] = meanFilterSNN(im[:,:,0], radius)
+    mean_snn[:,:,1] = meanFilterSNN(im[:,:,1], radius)
+    mean_snn[:,:,2] = meanFilterSNN(im[:,:,2], radius)
+
     plt.figure()
-    im_snn = Image.fromarray(mean_swf.astype('uint8'))
+    im_snn = Image.fromarray(mean_snn.astype('uint8'))
     plt.imshow(im_snn)
+    plt.axis('off')
+    plt.show()
+
+    # Kuwahara
+    mean_kuwahara = np.zeros(im.shape, im.dtype)
+    mean_kuwahara[:,:,0] = meanFilterKuwahara(im[:,:,0], radius)
+    mean_kuwahara[:,:,1] = meanFilterKuwahara(im[:,:,1], radius)
+    mean_kuwahara[:,:,2] = meanFilterKuwahara(im[:,:,2], radius)
+
+    plt.figure()
+    im_kuwahara = Image.fromarray(mean_kuwahara.astype('uint8'))
+    plt.imshow(im_kuwahara)
     plt.axis('off')
     plt.show()
 
